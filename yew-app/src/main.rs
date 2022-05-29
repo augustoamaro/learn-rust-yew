e78fcfd6551e166b71ae4f747ceb8c805acfc379
Leonidas Augusto Amaro <augustoamaro@icloud.com>
2022-05-28 21:57:41 -0300

    Added counter example

--- yew-app/src/main.rs
+++ yew-app/src/main.rs
@@ -1,9 +1,41 @@
 use yew::prelude::*;
 
-struct Model {
-    value: i64,
+enum Msg {
+    AddOne
 }
 
-fn app() -> Html {
-    let state = use_state(|| Model { value: 0 });
+struct CounterComponent{
+    count: i64,
+}
+
+impl Component for CounterComponent {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {count: 0}
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.count += 1;
+                true // re-render component
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+        html! {
+            <div class="container">
+                <p>{ self.count }</p>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<CounterComponent>();
 }
